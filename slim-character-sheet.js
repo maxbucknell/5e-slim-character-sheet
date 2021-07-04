d85c50a71efae66dac7de60e3c709f14ecaa7e58
Max Bucknell <me@maxbucknell.com>
2021-07-04 11:15:30 +0000

    Set default height correctly

--- slim-character-sheet.js
+++ slim-character-sheet.js
@@ -1,6 +1,13 @@
 import OriginalCharacterSheet from "../../systems/dnd5e/module/actor/sheets/character.js"
 
 export class SlimCharacterSheet extends OriginalCharacterSheet {
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ['dnd5e', 'sheet', 'actor', 'character', 'slim'],
+            height: 200
+        })
+    }
+
     get template() {
         if ( !game.user.isGM && this.actor.limited ) {
             return "systems/dnd5e/templates/actors/limited-sheet.html"
